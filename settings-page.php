9a1c857b871d35f45d8b69fa79950c6223a10529
Sergiy Matiychuk <seriyyy95@gmail.com>
2020-06-11 16:54:32 +0300

    Add post id field to settings page

--- settings-page.php
+++ settings-page.php
@@ -1,6 +1,7 @@
 <h2>Записи с общим доступом:</h2>
 <table>
 <thead>
+<th>Post ID</th>
 <th>Запись</th>
 <th>Пользователь</th>
 <th>Действия</th>
@@ -11,6 +12,7 @@
 	<?php $user_info = get_userdata($shared_data["user_id"]);?>
 	<?php $user_login = $user_info->user_login; ?>
 	<tr>
+		<td><?php echo $shared_data["post_id"]; ?></td>
 		<td><?php echo $post_title; ?></td>
 		<td><?php echo $user_login; ?></td>
 		<td><a href="<?php echo admin_url( 'admin-post.php' ); ?>?action=shareaccess_unshare&id=<?php echo $shared_data["id"]; ?>">Удалить</a></td>
